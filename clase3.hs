9d565b7bf726e73faecf263987129b2b73db82fa
beniteznahuel <beniteznahueloscar@gmail.com>
2014-03-28 11:32:29 -0300

    Practica3 tree2list agregado

--- clase3.hs
+++ clase3.hs
@@ -10,4 +10,19 @@ esHoja _ = False
 
 countLeaves :: Tree a -> Int
 countLeaves EmptyT = 0
-countLeaves (NodeT r ti td) = if (esHoja (NodeT r ti td)) then 1 else (countLeaves ti) + (countLeaves td)
+countLeaves t@(NodeT _ ti td) = if esHoja t then 1 else countLeaves ti + countLeaves td
+
+height :: Tree a -> Int
+height EmptyT = 0
+height (NodeT r t1 t2) = 1 + max (height t1) (height t2)
+
+minT :: Tree Int -> Int
+minT EmptyT = error "no tiene"
+minT (NodeT r EmptyT EmptyT) = r 
+minT (NodeT r EmptyT t2) = min r (minT t2)
+minT (NodeT r t1 EmptyT) = min r (minT t1)
+minT (NodeT r t1 t2) = min r (min (minT t1) (minT t2))
+
+tree2list :: Tree a -> [a]
+tree2list EmptyT = []
+tree2list (NodeT r t1 t2) = r : ((tree2list t1) ++ (tree2list t2))
